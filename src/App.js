2e07d32fcd47a9e7d8cfe2b01b567aa16268772d
Aparna <aparnaprasad106@gmail.com>
2023-05-11 12:58:23 -0700

    netflixapp

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import './App.css';
+import Banner from './componnets/Banner';
+import Nav from './componnets/Nav';
 import Row from './componnets/Row';
 import requests from './request';
 
 function App() {
   return (
     <div className="App">
-      <h1>NETFLIX</h1>
-      <Row title={'Netflix Orginals'} fetchURL={requests.fetchNetflixOriginals} />
+      <Nav />
+      <Banner />
+      <Row isPoster={true} title={'Netflix Orginals'} fetchURL={requests.fetchNetflixOriginals} />
+      <Row title={'Trending'} fetchURL={requests.fetchTrending} />
+      <Row title={'Top Rated'} fetchURL={requests.fetchTopRated} />
+      <Row title={'Action'} fetchURL={requests.fetchActionMovies} />
+      <Row title={'Comedy'} fetchURL={requests.fetchComedyMovies} />
+      <Row title={'Horror'} fetchURL={requests.fetchHorrorMovies} />
+      <Row title={'Romance'} fetchURL={requests.fetchRomanceMovies} />
+      <Row title={'Documentaries'} fetchURL={requests.fetchDocumentaries} />
+
     </div>
   );
 }
--- src/componnets/Row.js
+++ src/componnets/Row.js
@@ -1,8 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import instance from '../instanceConfig'
+import './Row.css'
+function Row({ isPoster,title,fetchURL}) {
 
-function Row() {
+    const base_url = "https://image.tmdb.org/t/p/original/";
+
+    const [movies,setMovies] = useState([])
+
+    const fetchData = async ()=>{
+        const response = await instance.get(fetchURL)
+        // destructuring
+        const {results} = response.data
+        setMovies(results)
+    }
+    console.log(movies);
+
+    useEffect(()=>{
+        fetchData()
+    },[])
+
+   
   return (
-    <div>Row</div>
+    <div className='row'>
+        <h2>{title}</h2>
+        <div className="movies_row">
+            {
+                movies.map((movie)=>(
+                    <img className={`movie ${isPoster&& 'poster'}`} src={`${base_url}/${isPoster?movie.poster_path:movie?.backdrop_path}`} alt="movie image" />
+                ))
+            }
+        </div>
+    </div>
   )
 }
 
